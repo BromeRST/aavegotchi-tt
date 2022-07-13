fa7aca93fe8c2568d5995904da23ecf79919d273
Massimiliano <94859244+BromeRST@users.noreply.github.com>
2022-07-13 18:30:19 +0200

    inverted grid

--- aavegotchi-tt-contracts/contracts/facets/GameFacet2.sol
+++ aavegotchi-tt-contracts/contracts/facets/GameFacet2.sol
@@ -1,4 +1,4 @@
- // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
 import {AppStorage, Modifiers, Match, Register, Tile} from "../libraries/AppStorage.sol";
@@ -10,9 +10,8 @@ import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "hardhat/console.sol";
 
 contract GameFacet2 is Modifiers {
+    event CardPlayed(uint256 indexed _matchId);
 
-    event CardPlayed (uint256 indexed _matchId);
- 
     function playCard(
         uint256 tokenId,
         uint256 matchId,
@@ -50,11 +49,11 @@ contract GameFacet2 is Modifiers {
         }
         require(x < 3, "GameFacet: wrong x");
         require(y < 3, "GameFacet: wrong y");
-        require(!s.grids[matchId][x][y].isActive, "GameFacet: wrong coords");
+        require(!s.grids[matchId][y][x].isActive, "GameFacet: wrong coords");
 
-        s.grids[matchId][x][y].isActive = true;
-        s.grids[matchId][x][y].tokenId = tokenId;
-        s.grids[matchId][x][y].winner = msg.sender;
+        s.grids[matchId][y][x].isActive = true;
+        s.grids[matchId][y][x].tokenId = tokenId;
+        s.grids[matchId][y][x].winner = msg.sender;
         s.matches[matchId].movsCounter++;
         // check around
         int16[6] memory playerGotchiParams = IAavegotchiDiamond(
@@ -63,55 +62,55 @@ contract GameFacet2 is Modifiers {
         int16[6] memory oppositeGotchiParams;
 
         if (
-            y != 0 &&
-            s.grids[matchId][x][y - 1].isActive &&
-            s.grids[matchId][x][y - 1].winner != msg.sender
-        ) {
-            uint256 oppositeTokenId = s.grids[matchId][x][y - 1].tokenId;
-            oppositeGotchiParams = IAavegotchiDiamond(s.aavegotchiDiamond)
-                .getAavegotchi(oppositeTokenId)
-                .modifiedNumericTraits;
-            if (playerGotchiParams[0] > oppositeGotchiParams[2]) {
-                s.grids[matchId][x][y - 1].winner = msg.sender;
-            }
-        }
-        if (
             x != 0 &&
-            s.grids[matchId][x - 1][y].isActive &&
-            s.grids[matchId][x - 1][y].winner != msg.sender
+            s.grids[matchId][y][x - 1].isActive &&
+            s.grids[matchId][y][x - 1].winner != msg.sender
         ) {
-            uint256 oppositeTokenId = s.grids[matchId][x - 1][y].tokenId;
+            uint256 oppositeTokenId = s.grids[matchId][y][x - 1].tokenId;
             oppositeGotchiParams = IAavegotchiDiamond(s.aavegotchiDiamond)
                 .getAavegotchi(oppositeTokenId)
                 .modifiedNumericTraits;
             if (playerGotchiParams[3] > oppositeGotchiParams[1]) {
-                s.grids[matchId][x - 1][y].winner = msg.sender;
+                s.grids[matchId][y][x - 1].winner = msg.sender;
             }
         }
         if (
-            y + 1 < 3 &&
-            s.grids[matchId][x][y + 1].isActive &&
-            s.grids[matchId][x][y + 1].winner != msg.sender
+            y != 0 &&
+            s.grids[matchId][y - 1][x].isActive &&
+            s.grids[matchId][y - 1][x].winner != msg.sender
         ) {
-            uint256 oppositeTokenId = s.grids[matchId][x][y + 1].tokenId;
+            uint256 oppositeTokenId = s.grids[matchId][y - 1][x].tokenId;
             oppositeGotchiParams = IAavegotchiDiamond(s.aavegotchiDiamond)
                 .getAavegotchi(oppositeTokenId)
                 .modifiedNumericTraits;
-            if (playerGotchiParams[2] > oppositeGotchiParams[0]) {
-                s.grids[matchId][x][y + 1].winner = msg.sender;
+            if (playerGotchiParams[0] > oppositeGotchiParams[2]) {
+                s.grids[matchId][y - 1][x].winner = msg.sender;
             }
         }
         if (
             x + 1 < 3 &&
-            s.grids[matchId][x + 1][y].isActive &&
-            s.grids[matchId][x + 1][y].winner != msg.sender
+            s.grids[matchId][y][x + 1].isActive &&
+            s.grids[matchId][y][x + 1].winner != msg.sender
         ) {
-            uint256 oppositeTokenId = s.grids[matchId][x + 1][y].tokenId;
+            uint256 oppositeTokenId = s.grids[matchId][y][x + 1].tokenId;
             oppositeGotchiParams = IAavegotchiDiamond(s.aavegotchiDiamond)
                 .getAavegotchi(oppositeTokenId)
                 .modifiedNumericTraits;
             if (playerGotchiParams[1] > oppositeGotchiParams[3]) {
-                s.grids[matchId][x + 1][y].winner = msg.sender;
+                s.grids[matchId][y][x + 1].winner = msg.sender;
+            }
+        }
+        if (
+            y + 1 < 3 &&
+            s.grids[matchId][y + 1][x].isActive &&
+            s.grids[matchId][y + 1][x].winner != msg.sender
+        ) {
+            uint256 oppositeTokenId = s.grids[matchId][y + 1][x].tokenId;
+            oppositeGotchiParams = IAavegotchiDiamond(s.aavegotchiDiamond)
+                .getAavegotchi(oppositeTokenId)
+                .modifiedNumericTraits;
+            if (playerGotchiParams[2] > oppositeGotchiParams[0]) {
+                s.grids[matchId][y + 1][x].winner = msg.sender;
             }
         }
 
@@ -126,7 +125,10 @@ contract GameFacet2 is Modifiers {
     }
 
     function checkWinner(uint256 matchId) internal {
-        require(s.matches[matchId].contested != true, "GameFacet2: already contested");
+        require(
+            s.matches[matchId].contested != true,
+            "GameFacet2: already contested"
+        );
         s.matches[matchId].contested = true;
         uint256 player1Points;
         uint256 player2Points;
@@ -141,14 +143,30 @@ contract GameFacet2 is Modifiers {
             }
         }
         if (player2Points < 4) {
-            IPool(s.aavePool).withdraw(s.dai, winnerPrize, s.matches[matchId].player1);
+            IPool(s.aavePool).withdraw(
+                s.dai,
+                winnerPrize,
+                s.matches[matchId].player1
+            );
             s.matches[matchId].winner = s.matches[matchId].player1;
         } else if (player2Points > 4) {
-            IPool(s.aavePool).withdraw(s.dai, winnerPrize, s.matches[matchId].player2);
+            IPool(s.aavePool).withdraw(
+                s.dai,
+                winnerPrize,
+                s.matches[matchId].player2
+            );
             s.matches[matchId].winner = s.matches[matchId].player2;
-        } else if (player2Points == 4 ) {
-            IPool(s.aavePool).withdraw(s.dai, s.matches[matchId].betsize * 1 ether, s.matches[matchId].player1);
-            IPool(s.aavePool).withdraw(s.dai, s.matches[matchId].betsize * 1 ether, s.matches[matchId].player2);
+        } else if (player2Points == 4) {
+            IPool(s.aavePool).withdraw(
+                s.dai,
+                s.matches[matchId].betsize * 1 ether,
+                s.matches[matchId].player1
+            );
+            IPool(s.aavePool).withdraw(
+                s.dai,
+                s.matches[matchId].betsize * 1 ether,
+                s.matches[matchId].player2
+            );
         }
 
         s.playersAmountStaked -= winnerPrize;
@@ -160,20 +178,35 @@ contract GameFacet2 is Modifiers {
     }
 
     function contestMatch(uint256 matchId) external {
-        require(block.timestamp >= s.matches[matchId].lastMove + 600 /*  259200 (3 days) */, "GameFacet2: not enough time");
-        require(s.matches[matchId].contested != true, "GameFacet2: already contested");
         require(
-            msg.sender == s.matches[matchId].player1 || 
-            msg.sender == s.matches[matchId].player2 || 
-            msg.sender == LibDiamond.contractOwner(), "GameFacet2: you are not allowed"
+            block.timestamp >= s.matches[matchId].lastMove + 600, /*  259200 (3 days) */
+            "GameFacet2: not enough time"
+        );
+        require(
+            s.matches[matchId].contested != true,
+            "GameFacet2: already contested"
+        );
+        require(
+            msg.sender == s.matches[matchId].player1 ||
+                msg.sender == s.matches[matchId].player2 ||
+                msg.sender == LibDiamond.contractOwner(),
+            "GameFacet2: you are not allowed"
         );
         uint256 winnerPrize = s.matches[matchId].betsize * 2 ether;
         s.matches[matchId].contested = true;
         if (s.matches[matchId].player2Turn) {
-            IPool(s.aavePool).withdraw(s.dai, winnerPrize, s.matches[matchId].player1);
+            IPool(s.aavePool).withdraw(
+                s.dai,
+                winnerPrize,
+                s.matches[matchId].player1
+            );
             s.matches[matchId].winner = s.matches[matchId].player1;
         } else {
-            IPool(s.aavePool).withdraw(s.dai, winnerPrize, s.matches[matchId].player2);
+            IPool(s.aavePool).withdraw(
+                s.dai,
+                winnerPrize,
+                s.matches[matchId].player2
+            );
             s.matches[matchId].winner = s.matches[matchId].player2;
         }
 
